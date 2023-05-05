055651a9e1734d05cd6be5f504198e52c32adaed
Staffan Ösp <staffan.osp@gmail.com>
2023-05-05 10:55:11 +0200

    status code fixes

--- backend/src/utils/utils.js
+++ backend/src/utils/utils.js
@@ -17,10 +17,12 @@ const fakeSlowServer = (delayMs) => {
 const fetchWithToken = async (...theArgs) => {
   const token = localStorage.getItem("token");
 
-  theArgs[1] = {
-    ...theArgs[1],
-    headers: { ...theArgs[1]?.headers, Authorization: `Bearer ${token}` },
-  };
+  if (token) {
+    theArgs[1] = {
+      ...theArgs[1],
+      headers: { ...theArgs[1]?.headers, Authorization: `Bearer ${token}` },
+    };
+  }
 
   return await fetch(...theArgs);
 };
--- backend/src/utils/verifyToken.js
+++ backend/src/utils/verifyToken.js
@@ -4,13 +4,13 @@ const verifyToken = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
-    return res.status(401).send("No token provided");
+    return res.sendStatus(401);
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
     if (err) {
       console.log(err);
-      return res.status(401).send("Invalid token");
+      return res.sendStatus(403);
     }
 
     req.token = token;
